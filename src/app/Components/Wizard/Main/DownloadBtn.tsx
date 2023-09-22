2c33c67a2bd00037f7184ab7a84a7d6ae84fbc02
Shlomi Shitrit <akunh113@gmail.com>
2023-09-22 09:27:15 +0300

    add option of creating a likert survey

--- src/app/Components/Wizard/Main/DownloadBtn.tsx
+++ src/app/Components/Wizard/Main/DownloadBtn.tsx
@@ -2,9 +2,13 @@
 import { saveAs } from "file-saver";
 import { Button } from "@/app/General/muiComponents";
 import { useSelector } from "react-redux";
-
-import { DownloadBtnProps } from "@/app/General/interfaces";
-
+import { RootState } from "@/app/store";
+import { replaceFirstAndLast } from "@/app/General/utils";
+import {
+    DownloadBtnProps,
+    MultiChoiceQuestion,
+    LikertQuestion,
+} from "@/app/General/interfaces";
 import {
     BLOB_TYPE,
     DOWNLOAD_BTN_TEXT,
@@ -14,7 +18,66 @@ import {
 } from "@/app/General/Resources/WizardMainRes";
 
 function DownloadBtn({ fileName = DOWNLOAD_BTN_EMPTY_STR }: DownloadBtnProps) {
-    const surveyParams = useSelector((state: any) => state.multiChoice);
+    const surveyType = useSelector(
+        (state: RootState) => state.stype.surveyType
+    );
+
+    // TODO: move to resources
+    const surveyParams = useSelector((state: RootState) => {
+        if (surveyType === "Multi Choice") {
+            return state.multiChoice;
+        } else if (surveyType === "Likert") {
+            return state.likert;
+        } else {
+            throw new Error("Invalid survey type");
+        }
+    });
+
+    // TODO: move to resources
+    let trial: string = "";
+    if (surveyType === "Multi Choice") {
+        const params = surveyParams as MultiChoiceQuestion[];
+        trial = `
+      const trial = {
+          type: jsPsychSurveyMultiChoice,
+          questions: [
+            {
+              prompt: "${params[0].promptQ}", 
+              name: "${params[0].nameQ}", 
+              options: [${params[0].optionsQ}], 
+              required: ${params[0].required},
+              horizontal: ${params[0].horizontal},
+            }, 
+          ],
+        };
+      `;
+    } else if (surveyType === "Likert") {
+        const params = surveyParams as LikertQuestion[];
+        const questions = params[0].promptQ.map((prompt, index) => {
+            return `{prompt: "${replaceFirstAndLast(
+                prompt,
+                "",
+                ""
+            )}", name: "${replaceFirstAndLast(
+                params[0].nameQ[index],
+                "",
+                ""
+            )}", labels: [${params[0].optionsQ}]}`;
+        });
+
+        trial = `
+      const trial = {
+          type: jsPsychSurveyLikert,
+          questions: [
+            ${questions} 
+          ],
+          randomize_question_order: ${params[0].randomQ},
+        };
+      `;
+    } else {
+        throw new Error("Invalid survey type");
+    }
+
     const fileContents = `
     <!DOCTYPE html>
     <html lang="en">
@@ -24,6 +87,9 @@ function DownloadBtn({ fileName = DOWNLOAD_BTN_EMPTY_STR }: DownloadBtnProps) {
             <title>JSPsych</title>
             <script src="https://unpkg.com/jspsych@7.3.3"></script>
             <script src="https://unpkg.com/@jspsych/plugin-survey-multi-choice@1.1.2"></script>
+            <script src="https://unpkg.com/@jspsych/plugin-survey-likert@1.1.2"></script>
+            <script src="https://unpkg.com/@jspsych/plugin-survey-text@1.1.2"></script>
+            <script src="https://unpkg.com/@jspsych/plugin-survey-multi-select@1.1.2"></script>
             <link href="https://unpkg.com/jspsych@7.3.3/css/jspsych.css" rel="stylesheet" type="text/css" />
           </head>
         </head>
@@ -32,18 +98,7 @@ function DownloadBtn({ fileName = DOWNLOAD_BTN_EMPTY_STR }: DownloadBtnProps) {
         const jsPsych = initJsPsych();
         const timeline = [];
 
-        const trial = {
-            type: jsPsychSurveyMultiChoice,
-            questions: [
-              {
-                prompt: "${surveyParams[0].promptQ}", 
-                name: "${surveyParams[0].nameQ}", 
-                options: [${surveyParams[0].optionsQ}], 
-                required: ${surveyParams[0].required},
-                horizontal: ${surveyParams[0].horizontal},
-              }, 
-            ],
-          };
+        ${trial}
 
         timeline.push(trial);
     
