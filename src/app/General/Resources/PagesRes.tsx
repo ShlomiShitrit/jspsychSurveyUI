d35f4b7a25597d3dc7a1cc0b9095b510894825ac
Shlomi Shitrit <akunh113@gmail.com>
2023-10-03 21:55:08 +0300

    Change terminology - Survey / question / form Fixed #18

--- src/app/General/Resources/PagesRes.tsx
+++ src/app/General/Resources/PagesRes.tsx
@@ -9,7 +9,7 @@ export const SEC_WELCOME_MSG =
 // CreatePage.tsx
 export const CREATE_TXT1 = "Welcome to the Survey Wizard!";
 export const CREATE_TXT2 = "Click the button below to get started";
-export const CREATE_BTN_TXT = "Create a Survey";
+export const CREATE_BTN_TXT = "Create questions block";
 export const CREATE_BTN_VARIANT = "contained";
 export const CREATE_BTN_COLOR = "success";
-export const CREATE_DOWNLOAF_BTN_TXT = "Download file";
+export const CREATE_DOWNLOAF_BTN_TXT = "Download Survey";
