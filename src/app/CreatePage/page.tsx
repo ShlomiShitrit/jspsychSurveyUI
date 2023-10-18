fa74a23b12ce44750a577118c84258e4db60d6fd
Shlomi Shitrit <akunh113@gmail.com>
2023-10-18 22:25:51 +0300

    Change the style Fixes #31

--- src/app/CreatePage/page.tsx
+++ src/app/CreatePage/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 import { Fragment, useState } from "react";
-import { ThemeProvider, Button, Container } from "@/app/General/muiComponents";
-import { darkNavyBlueTheme, createPageBtnStyle } from "@/app/General/styles";
+import {
+    ThemeProvider,
+    Button,
+    Container,
+    Stack,
+} from "@/app/General/muiComponents";
+import {
+    darkNavyBlueTheme,
+    createPageBtnStyle,
+    welcomeMsgStackStyle,
+} from "@/app/General/styles";
 import { Provider } from "react-redux";
 import SurveysList from "@/app/Components/UI/SurveysList";
 import WizardDialog from "@/app/Components/Wizard/WizardDialog";
@@ -9,6 +18,7 @@ import WelcomeMessage from "@/app/Components/UI/WelcomeMsg";
 import DownloadDialog from "@/app/Components/Forms/DownloadDialog";
 import store from "@/app/store/index";
 import VersionSelect from "@/app/Components/Forms/VersionSelect";
+import styles from "@/app/LandingPage.module.css";
 import {
     CREATE_TXT1,
     CREATE_TXT2,
@@ -43,7 +53,7 @@ function CreatePage() {
         <Provider store={store}>
             <ThemeProvider theme={darkNavyBlueTheme}>
                 <Fragment>
-                    <Container maxWidth={CONTAINER_MAX_WIDTH}>
+                    <Container className={styles.root}>
                         <WizardDialog
                             open={openWizrad}
                             closeWizard={closeWizard}
@@ -58,18 +68,34 @@ function CreatePage() {
                             text2={CREATE_TXT2}
                             btnText={CREATE_BTN_TXT}
                         />
+
                         <br />
-                        <br />
-                        <VersionSelect />
-                        <SurveysList />
-                        <Button
-                            sx={createPageBtnStyle}
-                            variant={CREATE_BTN_VARIANT}
-                            color={CREATE_BTN_COLOR}
-                            onClick={openDownloadDialog}
+                        <Stack
+                            sx={welcomeMsgStackStyle}
+                            direction={"row"}
+                            spacing={2}
+                            justifyContent={"center"}
                         >
-                            {CREATE_DOWNLOAF_BTN_TXT}
-                        </Button>
+                            <VersionSelect />
+                        </Stack>
+
+                        <SurveysList />
+                        <Stack
+                            sx={welcomeMsgStackStyle}
+                            direction={"row"}
+                            spacing={2}
+                            justifyContent={"center"}
+                        >
+                            <Button
+                                className={styles.button}
+                                sx={createPageBtnStyle}
+                                variant={CREATE_BTN_VARIANT}
+                                color={CREATE_BTN_COLOR}
+                                onClick={openDownloadDialog}
+                            >
+                                {CREATE_DOWNLOAF_BTN_TXT}
+                            </Button>
+                        </Stack>
                     </Container>
                 </Fragment>
             </ThemeProvider>
