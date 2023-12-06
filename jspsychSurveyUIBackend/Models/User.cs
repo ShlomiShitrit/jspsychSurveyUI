570ab2ef7c285f33a92344541a1a05b9c1a29bd7
Shlomi Shitrit <akunh113@gmail.com>
2023-12-06 12:57:40 +0200

    create an API

---
+++ jspsychSurveyUIBackend/Models/User.cs
@@ -0,0 +1,12 @@
+namespace jspsychSurveyUIBackend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public ICollection<Trial> Trials { get; set; }
+    }
+}
